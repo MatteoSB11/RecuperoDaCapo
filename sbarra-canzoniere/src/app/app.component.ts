ee47ea482ffc2eee5c532bf259618095e48e1c66
MatteoSB11 jgjg <matteosbarra07@gmail.com>
2025-05-30 14:06:14 +0000

    Punto 3

--- sbarra-canzoniere/src/app/app.component.ts
+++ sbarra-canzoniere/src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs/internal/Observable';
+import { Canzoni } from './models/song.model';
+import { HttpClient } from '@angular/common/http';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sbarra-canzoniere';
+  data! : Object
+  loading! : boolean
+  obs! : Observable<Object>
+  vettCanzoni! : Canzoni[];
+  obsCanzoni! : Observable<Canzoni[]>
+
+  constructor(public http : HttpClient){}
+
+  makeRichiesta() : void{
+    this.loading = true;
+    this.obs = this.http.get<Canzoni[]>('https://my-json-server.typicode.com/malizia-g/hotel/short-songlist');
+    this.obs.subscribe(this.getData);
+  }
+  getData = (d : Object) => {
+    this.data = d;
+    this.loading = false;
+  }
+  makeTypedRequest() : void {
+  this.obsCanzoni = this.http.get<Canzoni[]>('https://my-json-server.typicode.com/malizia-g/hotel/short-songlist'); 
+  this.obsCanzoni.subscribe(data => {
+      this.vettCanzoni = data;
+      console.log("Dati canzoni caricati:", this.vettCanzoni); 
+  });
+ }
 }
--- sbarra-canzoniere/src/app/models/song.model.ts
+++ sbarra-canzoniere/src/app/models/song.model.ts
@@ -1,3 +1,3 @@
 export class Canzoni{
-    
+    constructor(public data_uscita : string, public durata : string, public title : string){}
 }
